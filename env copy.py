20cdd98ded66d93ea70d992ba7843ad3679bc646
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-09-27 12:56:32 +0000

    Add materialized navbar

--- env copy.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://julian:password@myfirstcluster.7m9mj.mongodb.net/task_manager?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
