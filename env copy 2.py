51e6c3891907bb112907f2a8d1bab5eaca46df12
juliandavid-garcia <juliandavid.garcia@gmail.com>
2021-09-28 18:03:20 +0000

    Add registerfunctionality

--- env copy 2.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "ACppq9Gkgj")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://julian:Amsterdam01@myfirstcluster.7m9mj.mongodb.net/task_manager?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "task_manager")
